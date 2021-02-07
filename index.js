16128552b66e1a5240137098578eabbe8fcd2229
Rafael Pirotto <pirotto@outlook.com>
2021-02-07 17:34:14 -0300

    on error function added

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var net = require("net");
+const net = require("net");
 
-var server = net.createServer(function (socket) {
-    socket.write("Echo server\r\n");
+const PORT = 1337;
+
+const server = net.createServer((socket) => {
+    // 'connection' listener.
+    console.log("client connected");
+    socket.on("end", () => {
+        console.log("client disconnected");
+    });
+    socket.write("server says hello\r\n");
     socket.pipe(socket);
 });
 
-server.listen(1337, "0.0.0.0");
+server.on("error", (err) => {
+    throw err;
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server listening in port ${PORT}`);
+});
