f18238947e47250306877cd957690c158a594201
Rafael Pirotto <pirotto@outlook.com>
2021-02-14 13:58:51 -0300

    removed hardcoded storage

--- Storage.js
+++ Storage.js
@@ -1,34 +1,7 @@
 class Storage {
     constructor() {
-        // this.storage = [];
+        this.storage = [];
         this.cas = 0;
-        // TODO: remove hardcoded storage
-        this.storage = [
-            {
-                key: "test1",
-                bytes: 2,
-                value: "25",
-                flags: 0,
-                exptime: 900,
-                cas: 1,
-            },
-            {
-                key: "test2",
-                bytes: 2,
-                value: "24",
-                flags: 0,
-                exptime: 900,
-                cas: 2,
-            },
-            {
-                key: "test3",
-                bytes: 2,
-                value: "23",
-                flags: 0,
-                exptime: 900,
-                cas: 3,
-            },
-        ];
     }
 
     find(key) {
@@ -41,6 +14,7 @@ class Storage {
 
     set({ key, value, flags, exptime, bytes }) {
         const objIndex = this.storage.findIndex((obj) => obj.key === key);
+        const found = objIndex !== -1;
         const objToInsert = {
             key,
             value,
@@ -48,7 +22,7 @@ class Storage {
             exptime,
             bytes,
         };
-        if (objIndex !== -1) {
+        if (found) {
             objToInsert.cas = this.storage[objIndex].cas;
             this.storage[objIndex] = objToInsert;
         } else {
