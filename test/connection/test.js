93a6ef854eef2659b678f7f137c8b877e1fca7f8
Rafael Pirotto <pirotto@outlook.com>
2021-02-20 19:24:42 -0300

    tests added to connection

--- test/connection/test.js
+++ test/connection/test.js
@@ -5,7 +5,7 @@ const { build } = require('../../domain/builder');
 const Connection = require('../../tcp/Connection');
 const { TERMINATOR, EMPTY_SPACE } = require('../../domain/constants/index');
 const { Set } = require('../../domain/commands');
-const DummySocket = require('../DummySocket/DummySocket');
+const DummySocket = require('../utils/DummySocket/DummySocket');
 
 const stringToBuffer = (stringRequest) => Buffer.from(stringRequest + TERMINATOR, 'utf8');
 
@@ -18,6 +18,11 @@ const testObj = {
   value: '22',
 };
 
+const key = 'new_key';
+const flags = 0;
+const exptime = 3600;
+const bytes = 2;
+
 beforeEach(() => {
   storage.initialize();
   storage.save(testObj);
@@ -26,53 +31,33 @@ beforeEach(() => {
 describe('builder', () => {
   describe('setExpectedData()', () => {
     describe('with expectedData', () => {
+      const socket = new DummySocket();
+      const dataString = `set ${key} ${flags} ${exptime} ${bytes}`;
+      const data = stringToBuffer(dataString);
+      const connection = new Connection(socket, data);
       describe('expected data from the set command', () => {
-        const key = 'new_key';
-        const flags = 0;
-        const exptime = 3600;
-        const bytes = 2;
-        const dataString = `set ${key} ${flags} ${exptime} ${bytes}`;
+        build(connection, storage);
         it('should return an instance of the set command', () => {
-          const socket = new DummySocket();
-          const data = stringToBuffer(dataString);
-          const connection = new Connection(socket, data);
-          build(connection, storage);
           const actual = socket.expectedData[0] instanceof Set;
           const expected = true;
           assert.strictEqual(actual, expected);
         });
         it('should return the corresponding key', () => {
-          const socket = new DummySocket();
-          const data = stringToBuffer(dataString);
-          const connection = new Connection(socket, data);
-          build(connection, storage);
           const actual = socket.expectedData[1];
           const expected = key;
           assert.strictEqual(actual, expected);
         });
         it('should return the corresponding flags', () => {
-          const socket = new DummySocket();
-          const data = stringToBuffer(dataString);
-          const connection = new Connection(socket, data);
-          build(connection, storage);
           const actual = socket.expectedData[2];
           const expected = flags;
           assert.strictEqual(actual, expected);
         });
         it('should return the corresponding exptime', () => {
-          const socket = new DummySocket();
-          const data = stringToBuffer(dataString);
-          const connection = new Connection(socket, data);
-          build(connection, storage);
           const actual = socket.expectedData[3];
           const expected = exptime;
           assert.strictEqual(actual, expected);
         });
         it('should return the corresponding bytes', () => {
-          const socket = new DummySocket();
-          const data = stringToBuffer(dataString);
-          const connection = new Connection(socket, data);
-          build(connection, storage);
           const actual = socket.expectedData[4];
           const expected = bytes;
           assert.strictEqual(actual, expected);
@@ -81,11 +66,11 @@ describe('builder', () => {
     });
 
     describe('without expected data', () => {
+      const socket = new DummySocket();
+      const data = stringToBuffer('get existing_key_1');
+      const connection = new Connection(socket, data);
+      build(connection, storage);
       it('should return undefined', () => {
-        const socket = new DummySocket();
-        const data = stringToBuffer('get existing_key_1');
-        const connection = new Connection(socket, data);
-        build(connection, storage);
         const actual = socket.expectedData;
         const expected = undefined;
         assert.strictEqual(actual, expected);
@@ -95,11 +80,11 @@ describe('builder', () => {
 
   describe('sendResponse()', () => {
     describe('nonempty response', () => {
+      const socket = new DummySocket();
+      const data = stringToBuffer('get non_existent_key');
+      const connection = new Connection(socket, data);
+      build(connection, storage);
       it('should return the corresponding nonempty response', () => {
-        const socket = new DummySocket();
-        const data = stringToBuffer('get non_existent_key');
-        const connection = new Connection(socket, data);
-        build(connection, storage);
         const actual = socket.text;
         const expected = `END${TERMINATOR}`;
         assert.strictEqual(actual, expected);
@@ -107,13 +92,37 @@ describe('builder', () => {
     });
 
     describe('empty response', () => {
+      const socket = new DummySocket();
+      const data = stringToBuffer('set new_key 0 3600 2');
+      const connection = new Connection(socket, data);
+      build(connection, storage);
       it('should return an empty space', () => {
-        const socket = new DummySocket();
-        const data = stringToBuffer('set new_key 0 3600 2');
-        const connection = new Connection(socket, data);
-        build(connection, storage);
         const actual = socket.text;
         const expected = EMPTY_SPACE;
+        assert.strictEqual(actual, expected);
+      });
+    });
+  });
+
+  describe('getExpectedData()', () => {
+    describe('without expectedData', () => {
+      const socket = new DummySocket();
+      const data = stringToBuffer('get key');
+      const connection = new Connection(socket, data);
+      it('should return undefined', () => {
+        const actual = connection.getExpectedData();
+        const expected = undefined;
+        assert.strictEqual(actual, expected);
+      });
+    });
+    describe('with expectedData', () => {
+      const socket = new DummySocket();
+      const data = stringToBuffer(`set ${key} ${flags} ${exptime} ${bytes}`);
+      const connection = new Connection(socket, data);
+      build(connection, storage);
+      it('should return an array of length 5 with the corresponding expectedData', () => {
+        const actual = connection.getExpectedData().length;
+        const expected = 5;
         assert.strictEqual(actual, expected);
       });
     });
