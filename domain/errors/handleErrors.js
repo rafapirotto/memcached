49a939ad5fbefd07dcb071d36ecabd6a99e00bbc
Rafael Pirotto <pirotto@outlook.com>
2021-02-24 15:13:52 -0300

    Server error added

--- domain/errors/handleErrors.js
+++ domain/errors/handleErrors.js
@@ -1,8 +1,19 @@
+const {
+  BadCommandLineError,
+  BadDataChunkError,
+  SyntaxError,
+} = require('./index');
+const { SERVER_ERROR } = require('../constants/messages');
+
 const handleErrors = (callback, errorCallback) => {
   try {
     callback();
   } catch (error) {
-    errorCallback(error.message);
+    const isClientError = error instanceof SyntaxError
+     || error instanceof BadDataChunkError
+     || error instanceof BadCommandLineError;
+    const message = isClientError ? error.message : SERVER_ERROR;
+    errorCallback(message);
   }
 };
 
